f3197ae39d2ed4c585e79267d9a78b9a2472fc63
Mateus CP <56638915+mateuscp22@users.noreply.github.com>
2022-11-01 15:26:15 -0300

    cursophpoo

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+class Pessoa
+{
+    public $nome;
+    public $idade;
+
+    public function Falar()
+    {
+        echo $this->nome . " de " . $this->idade . " acabou de falar";
+    }
+}
+
+$rodrigo = new Pessoa();
+$rodrigo->nome = "Mateus";
+$rodrigo->idade = 25;
+$rodrigo->Falar();
